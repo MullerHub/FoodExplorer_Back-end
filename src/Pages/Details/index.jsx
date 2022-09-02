e73a6f65bc879c9cba67f4789ae100bcb9a01d98
Murilo_Muller <murilomuller@protonmail.com>
2022-09-01 23:02:02 -0300

    ajustes iniciais¹

---
+++ src/Pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles"
+
+export function Details() {
+
+  return (
+    <>
+      <Container className={Container}>"Teste um"
+        <p>teste de componente</p>
+      </Container>
+    </>
+  )
+}
