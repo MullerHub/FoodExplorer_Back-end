9ff5901e20ae494b326b4c7b97441938acd0da18
Murilo Muller <murilomuller@protonmail.com>
2023-05-04 01:12:14 -0300

    App Error implemented in app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,43 @@
 require('express-async-errors')
+
 const express = require('express');
 const adminController = require('./controllers/adminController');
 const AppError = require('./utils/AppError')
+
 const app = express();
+
 
 const routes = require('./routes')
 const cors = require('cors')
 
 const path = require('path')
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((error, response, request, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: "Internal server error"
+  })
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
 
 app.get('/admin', (req, res) => {
   res.send('This is the admin page.');
