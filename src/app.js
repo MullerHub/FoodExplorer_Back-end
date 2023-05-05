e937227d1e7075fc2be25c108acb312ffd9ec743
Murilo Muller <murilomuller@protonmail.com>
2023-05-05 15:07:01 -0300

    App.js adjusts

--- src/app.js
+++ src/app.js
@@ -3,17 +3,13 @@ require('express-async-errors')
 const express = require('express');
 const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError')
-const UsersController = require('./controllers/UsersController');
-const path = require('path');
 
 const routes = require('./routes');
-const cors = require('cors');
 
 migrationsRun();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(routes);
 
 app.use((error, response, request, next) => {
