7852d6a438f3a1883204ca74bbf1ffbac63bcf63
Murilo Muller <murilomuller@protonmail.com>
2023-05-13 23:11:21 -0300

    favorite_plates basic add

--- src/routes/plates.routes.js
+++ src/routes/plates.routes.js
@@ -8,6 +8,7 @@ const platesController = new PlatesController();
 
 platesRoutes.post("/:user_id", platesController.create);
 platesRoutes.get("/:id", platesController.show);
+nb;
 
 // platesRoutes.put("/:id", platesController.update);
 
