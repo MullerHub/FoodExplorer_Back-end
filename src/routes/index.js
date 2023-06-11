3e0e0b97d7105411674d5d111646bdaf97dce115
Murilo Da Silva Muller <murilomuller@protonmail.com>
2023-06-11 17:52:26 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const usersRoutes = require("./users.routes");
 const platesRoutes = require("./plates.routes");
 const favoriteRoutes = require("./favorite.routes");
 const sessionsRoutes = require("./sessions.routes");
+const ingredientsRoutes = require("./ingredients.routes");
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
 routes.use("/plates", platesRoutes);
 routes.use("/favorites", favoriteRoutes);
+routes.use("/ingredients", ingredientsRoutes);
 
 module.exports = routes;
